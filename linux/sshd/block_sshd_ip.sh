8b32e75e33252a0f8a1f4db72ca63eba7fce4f62
vanilla <rose20.99.c@gmail.com>
2022-01-14 14:21:28 +0800

    [update]

---
+++ linux/sshd/block_sshd_ip.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+lastb | grep "ssh:notty" | awk '{print $(NF-7)}' |sort|uniq -c | awk '{print $2"="$1;}' > /app/local/black.list
+for i in `cat /app/local/black.list`
+do
+  IP=`echo $i |awk -F= '{print $1}'`
+  NUM=`echo $i|awk -F= '{print $2}'`
+  if [ ${#NUM} -gt 10 ]; then
+    grep $IP /etc/hosts.deny > /dev/null
+    if [ $? -gt 0 ];then
+      echo "sshd:$IP:deny" >> /etc/hosts.deny
+    fi
+  fi
+done
