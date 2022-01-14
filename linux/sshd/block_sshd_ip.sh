c2bb5fb6c0bc1f386d504930dc721c671295a120
vanilla <rose20.99.c@gmail.com>
2022-01-14 14:50:49 +0800

    update

--- linux/sshd/block_sshd_ip.sh
+++ linux/sshd/block_sshd_ip.sh
@@ -4,7 +4,7 @@ for i in `cat /tmp/black.list`
 do
   IP=`echo $i |awk -F= '{print $1}'`
   NUM=`echo $i|awk -F= '{print $2}'`
-  if [ ${#NUM} -gt 10 ]; then
+  if [ ${NUM} -gt 10 ]; then
     grep $IP /etc/hosts.deny > /dev/null
     if [ $? -gt 0 ];then
       echo "sshd:$IP:deny" >> /etc/hosts.deny
