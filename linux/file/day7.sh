3fe994596e04b441519c4c894179dd314ac0c202
Angelia <rose20.99.c@gmail.com>
2022-06-21 21:03:40 +0800

    d

--- linux/file/day7.sh
+++ linux/file/day7.sh
@@ -2,10 +2,10 @@
 #
 while true
 do
-    # 每天检查一次
-    sleep 24h
     # 定位到日志文件目录
     log_dir=/var/log
     # 删除文件
-    find $log_dir -type f -mtime +7 -exec rm -rf {} \;
+    find $log_dir -type f -mtime +7 -exec rm -rf {} \;  
+    # 每天检查一次
+    sleep 24h
 done
